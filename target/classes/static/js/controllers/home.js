77143d51ec196be3fa703f4c004fa98654e8d0c2
Kristina <k.roncevic90@gmail.com>
2016-06-02 21:06:08 +0200

    Translation

--- target/classes/static/js/controllers/home.js
+++ target/classes/static/js/controllers/home.js
@@ -1,6 +1,6 @@
 angular.module('home', []).controller('home', function($http, $scope) {
 	var self = this;
-	$scope.sharedData.title = 'Home';
+	$scope.sharedData.title = 'Naslovnica';
 	$http.get('rest/user/').then(function(response) {
 		self.user = response.data.name;
 		self.authorization = response.data.authorities[0].authority;
--- target/classes/static/js/controllers/profile.js
+++ target/classes/static/js/controllers/profile.js
@@ -1,6 +1,6 @@
 angular.module('profile', []).controller('profile', function($http, $scope) {
 	var self = this;
-	$scope.sharedData.title = 'Profile';
+	$scope.sharedData.title = 'Profil';
 	$http.get('rest/user/').then(function(response) {
 		self.user = response.data.name;
 		self.authorization = response.data.authorities[0].authority;
