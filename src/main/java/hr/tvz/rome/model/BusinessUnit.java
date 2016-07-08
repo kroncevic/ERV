20041b1827db0053b1bbd75a5dcf55d07c1cacc6
Kristina <k.roncevic90@gmail.com>
2016-07-08 11:17:13 +0200

    BusinessUnit model implementation - bug fixed

--- src/main/java/hr/tvz/rome/model/BusinessUnit.java
+++ src/main/java/hr/tvz/rome/model/BusinessUnit.java
@@ -2,16 +2,13 @@ package hr.tvz.rome.model;
 
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
